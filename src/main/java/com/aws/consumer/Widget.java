fa359a0d6ced269d2afaf20e0eedac1dfee2979f
arunvemireddy <vemireddyarun0701@gmail.com>
2023-10-16 17:00:35 -0600

    fc

---
+++ src/main/java/com/aws/consumer/Widget.java
@@ -0,0 +1,44 @@
+package com.aws.consumer;
+
+import java.util.List;
+
+public class Widget {
+
+
+    private String id;
+    private String owner;
+    private String description;
+    private List<OtherAttribute> otherAttributes;
+    
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<OtherAttribute> getOtherAttributes() {
+		return otherAttributes;
+	}
+	public void setOtherAttributes(List<OtherAttribute> otherAttributes) {
+		this.otherAttributes = otherAttributes;
+	}
+	@Override
+	public String toString() {
+		return "Widget [id=" + id + ", owner=" + owner + ", description=" + description + ", otherAttributes="
+				+ otherAttributes + "]";
+	}
+
+}
